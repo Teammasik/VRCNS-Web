9ad2e097eb7343d9a17cb08a33fad162a9960ab3
Teammasik <ai.work.lrng@gmail.com>
2023-09-14 23:30:45 +0500

    recreated connection
    - rn there should be no timeouts
    - deleted connect func
    - added func to connect.js which makes connection every time
    - added few commentaries

--- Web/connection.js
+++ Web/connection.js
@@ -2,13 +2,15 @@ import { createRequire } from "module";
 
 
 const require = createRequire(import.meta.url);
-const mysql = require('mysql');
-
-
+export const mysql = require('mysql');
 export let PORT = 8080;
-export const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "unityaccess",
-    password: "root",
-});
+
+
+export function get_connection(){
+    return mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "unityaccess",
+        password: "root",
+    })
+}
--- Web/index.js
+++ Web/index.js
@@ -3,8 +3,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url); // those two rows make "require" work simultaneously with "import"
 
 
-import {PORT} from "./connection.js";
-import {conn} from "./connection.js";
+import {PORT, get_connection} from "./connection.js"; // get_connection is a pool, if I'm not mistaken
 
 
 const express = require('express');
@@ -19,17 +18,6 @@ app.use(
         origin: "*",
     })
 )
-
-
-conn.connect(err=>{
-    if(err){
-        console.log(err);
-        return err;
-    }
-    else{
-        console.log('Db is ok')
-    }
-});
 
 
 // example of json file
@@ -49,6 +37,7 @@ app.post('/sendData', (req,res) => {
     let query1 = "insert into students(userName,userSurname, userGroup, mark, uDate, points, test) VALUES (?,?,?,?,?,?,?)";
     conn.query(query1, [req.body.name, req.body.surname,req.body.group,req.body.mark,req.body.udate,req.body.points,req.body.test] ,(err, result) =>{
         res.send(req.body);
+        console.log("successfully sent data ")
     });
 })
 
@@ -56,13 +45,16 @@ app.post('/sendData', (req,res) => {
 // method gets everything from main table
 // http://217.18.60.195:8080/tshirt
 app.get('/data', (req, res) =>{
-    let query1 = "SELECT * FROM `students`";
-    conn.query(query1, (err, result) =>{
-            res.status(200).send({
-                result
-            })
-        });
-
+    var conn = get_connection()
+    conn.connect(function (err){
+        let query1 = "SELECT * FROM `students`";
+        conn.query(query1, (err, result) =>{
+                res.status(200).send({
+                    result
+                });
+                console.log("request data completed successfully");
+            });
+    });
 });
 
 
@@ -86,18 +78,21 @@ app.get('/tshirt', (req, res) => {
     res.status(200).send({
         tshirt: 'ss',
         size: 'large'
-    })
+    });
 });
 
 
 // http://217.18.60.195:8080/points
 app.get('/points', (req, res) => {
-    let query2 = "SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<11 UNION SELECT DISTINCT COUNT(userName) FROM `students` WHERE points<16 UNION SELECT DISTINCT COUNT(userName) FROM `students` WHERE points<21 UNION SELECT DISTINCT COUNT(userName) FROM `students` WHERE points<26"
-    conn.query(query2, (err, result) =>{
-        res.status(200).send({
-            result
-        })
-        console.log("request points completed successfully")
+    var conn = get_connection()
+    conn.connect(function (err){
+        let query2 = "SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<11 UNION SELECT DISTINCT COUNT(userName) FROM `students` WHERE points<16 UNION SELECT DISTINCT COUNT(userName) FROM `students` WHERE points<21 UNION SELECT DISTINCT COUNT(userName) FROM `students` WHERE points<26"
+        conn.query(query2, (err, result) =>{
+            res.status(200).send({
+                result
+            })
+            console.log("request points completed successfully")
+        });
     });
 });
 
@@ -105,70 +100,83 @@ app.get('/points', (req, res) => {
 // http://217.18.60.195:8080/attempts
 // delete in final build
 app.get('/attempts', (req, res) => {
-    let query3 = "SELECT COUNT(*) as students, amount as attempts FROM (SELECT count(*) as amount FROM `students`GROUP by userName) as sss GROUP BY amount"
-    conn.query(query3, (err, result) =>{
-        let studs = new Array;
-        let marks = new Array;
-        var dict1 = {};
-        for (let i = 0; i < result.length; i++) {
-            studs.push(result[i].students);
-          }
-        for (let i = 0; i < result.length; i++) {
-            marks.push(result[i].attempts);
-          }
+    var conn = get_connection()
+    conn.connect(function (err){
+        let query3 = "SELECT COUNT(*) as students, amount as attempts FROM (SELECT count(*) as amount FROM `students`GROUP by userName) as sss GROUP BY amount"
+        conn.query(query3, (err, result) =>{
+            let studs = new Array;
+            let marks = new Array;
+            var dict1 = {};
+            for (let i = 0; i < result.length; i++) {
+                studs.push(result[i].students);
+            }
+            for (let i = 0; i < result.length; i++) {
+                marks.push(result[i].attempts);
+            }
 
 
-        var dict1 = {
-            name: marks,
-            value: studs
-        }
+            var dict1 = {
+                name: marks,
+                value: studs
+            }
 
-        result.push(dict1)
-        res.status(200).send({
-            result
-        })
+            result.push(dict1)
+            res.status(200).send({
+                result
+            })
+            console.log("request attempts completed successfully")
+        });
     });
 });
 
 
 // http://217.18.60.195:8080/fetchbyid/1
 app.get('/fetchById/:id', (req, res) => {
-    const fetchid=req.params.id;
-    conn.query('select id, userName, userSurname, userGroup from `students` where id=?', fetchid,(err, result) =>{
-        res.status(200).send({result})
-        console.log("request fetchById completed successfully")
+    var conn = get_connection()
+    conn.connect(function (err){
+        const fetchid=req.params.id;
+        conn.query('select id, userName, userSurname, userGroup from `students` where id=?', fetchid,(err, result) =>{
+            res.status(200).send({result})
+            console.log("request fetchById completed successfully")
+        });
     });
 });
 
 
 // http://217.18.60.195:8080/testResults/1
 app.get('/testResults/:test', (req, res) => { 
-    const fetchid=req.params.test;
-    conn.query("SELECT id, userName, userSurname, userGroup, mark FROM `students` where test = ?", fetchid,(err, result) =>{
-        res.status(200).send({result})
-        console.log("request testResults completed successfully")
+    var conn = get_connection()
+    conn.connect(function (err){
+        const fetchid=req.params.test;
+        conn.query("SELECT id, userName, userSurname, userGroup, mark FROM `students` where test = ?", fetchid,(err, result) =>{
+            res.status(200).send({result})
+            console.log("request testResults completed successfully")
+        });
     });
 });
 
 
 // http://217.18.60.195:8080/testScore/1
 app.get('/testScore/:test', (req, res) => {
-    const fetchid=req.params.test;
-    conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<11 and test = ?', fetchid,(err, result) =>{
-        conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<16 and points>10 and test = ?', fetchid,(err, result1) =>{
-            conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<21 and points>15 and test = ?', fetchid,(err, result2) =>{
-                conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<26 and points>20 and test = ?', fetchid,(err, result3) =>{
-                    var answer = {
-                        test: fetchid,
-                        data: {
-                            10: result[0].pnts,
-                            15: result1[0].pnts,
-                            20: result2[0].pnts,
-                            25: result3[0].pnts,
+    var conn = get_connection()
+    conn.connect(function (err){
+        const fetchid=req.params.test;
+        conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<11 and test = ?', fetchid,(err, result) =>{
+            conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<16 and points>10 and test = ?', fetchid,(err, result1) =>{
+                conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<21 and points>15 and test = ?', fetchid,(err, result2) =>{
+                    conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<26 and points>20 and test = ?', fetchid,(err, result3) =>{
+                        var answer = {
+                            test: fetchid,
+                            data: {
+                                10: result[0].pnts,
+                                15: result1[0].pnts,
+                                20: result2[0].pnts,
+                                25: result3[0].pnts,
+                            }
                         }
-                    }
-                    res.status(200).send({answer})
-                    console.log("request testScore completed successfully")
+                        res.status(200).send({answer})
+                        console.log("request testScore completed successfully")
+                    });
                 });
             });
         });
@@ -178,24 +186,27 @@ app.get('/testScore/:test', (req, res) => {
 
 // http://217.18.60.195:8080/testPercentResult/1
 app.get('/testPercentResult/:test', (req, res) => {
-    const fetchid=req.params.test;
-    var summ
-    conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<11 and test = ?', fetchid,(err, result) =>{
-        conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<16 and points>10 and test = ?', fetchid,(err, result1) =>{
-            conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<21 and points>15 and test = ?', fetchid,(err, result2) =>{
-                conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<26 and points>20 and test = ?', fetchid,(err, result3) =>{
-                    summ = result[0].pnts + result1[0].pnts + result2[0].pnts + result3[0].pnts
-                    var answer = {
-                        test: fetchid,
-                        data: {
-                            10: Math.round(result[0].pnts/summ*100),
-                            15: Math.round(result1[0].pnts/summ*100),
-                            20: Math.round(result2[0].pnts/summ*100),
-                            25: Math.round(result3[0].pnts/summ*100),
+    var conn = get_connection()
+    conn.connect(function (err){
+        const fetchid=req.params.test;
+        var summ
+        conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<11 and test = ?', fetchid,(err, result) =>{
+            conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<16 and points>10 and test = ?', fetchid,(err, result1) =>{
+                conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<21 and points>15 and test = ?', fetchid,(err, result2) =>{
+                    conn.query('SELECT DISTINCT COUNT(userName) as pnts FROM `students` WHERE points<26 and points>20 and test = ?', fetchid,(err, result3) =>{
+                        summ = result[0].pnts + result1[0].pnts + result2[0].pnts + result3[0].pnts
+                        var answer = {
+                            test: fetchid,
+                            data: {
+                                10: Math.round(result[0].pnts/summ*100),
+                                15: Math.round(result1[0].pnts/summ*100),
+                                20: Math.round(result2[0].pnts/summ*100),
+                                25: Math.round(result3[0].pnts/summ*100),
+                            }
                         }
-                    }
-                    res.status(200).send({answer})
-                    console.log("request testPercentResult completed successfully")
+                        res.status(200).send({answer})
+                        console.log("request testPercentResult completed successfully")
+                    });
                 });
             });
         });
@@ -205,12 +216,15 @@ app.get('/testPercentResult/:test', (req, res) => {
 
 // http://217.18.60.195:8080/testList
 app.get('/testList', (req, res) => {
-    let query2 = "SELECT id, test FROM `tests`"
-    conn.query(query2, (err, result) =>{
-        res.status(200).send({
-            result
-        })
-        console.log("request testList completed successfully")
+    var conn = get_connection()
+    conn.connect(function (err){
+        let query2 = "SELECT id, test FROM `tests`"
+        conn.query(query2, (err, result) =>{
+            res.status(200).send({
+                result
+            })
+            console.log("request testList completed successfully")
+        });
     });
 });
 
